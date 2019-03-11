c313963652036b4492fd55de07c45723e8909b78
Anindita Basak <anindita.basak@live.com>
2019-03-11 15:38:38 +0530

    Add files via upload

---
+++ Day1/HOL/Lab5/Create-ASA.ps1
@@ -0,0 +1,58 @@
+﻿$storageAccountName = "myasaquickstartstorage"
+$storageAccount = New-AzStorageAccount `
+  -ResourceGroupName $resourceGroup `
+  -Name $storageAccountName `
+  -Location $location `
+  -SkuName Standard_LRS `
+  -Kind Storage
+
+$ctx = $storageAccount.Context
+$containerName = "container1"
+
+New-AzStorageContainer `
+  -Name $containerName `
+  -Context $ctx
+
+$storageAccountKey = (Get-AzStorageAccountKey `
+  -ResourceGroupName $resourceGroup `
+  -Name $storageAccountName).Value[0]
+
+Write-Host "The <storage account key> placeholder needs to be replaced in your output json files with this key value:"
+Write-Host $storageAccountKey -ForegroundColor Cyan
+
+#Create Stream Analytics Job
+
+$jobName = "MyStreamingJob"
+$jobDefinitionFile = "C:\JobDefinition.json"
+New-AzStreamAnalyticsJob `
+  -ResourceGroupName $resourceGroup `
+  -File $jobDefinitionFile `
+  -Name $jobName `
+  -Force
+
+  #Configure Input
+  $jobInputName = "IoTHubInput"
+$jobInputDefinitionFile = "C:\JobInputDefinition.json"
+New-AzStreamAnalyticsInput `
+  -ResourceGroupName $resourceGroup `
+  -JobName $jobName `
+  -File $jobInputDefinitionFile `
+  -Name $jobInputName
+
+  #Configure Output
+  $jobOutputName = "BlobOutput"
+$jobOutputDefinitionFile = "C:\JobOutputDefinition.json"
+New-AzStreamAnalyticsOutput `
+  -ResourceGroupName $resourceGroup `
+  -JobName $jobName `
+  -File $jobOutputDefinitionFile `
+  -Name $jobOutputName -Force
+
+  #Job Transformation Query
+  $jobTransformationName = "MyJobTransformation"
+$jobTransformationDefinitionFile = "C:\JobTransformationDefinition.json"
+New-AzStreamAnalyticsTransformation `
+  -ResourceGroupName $resourceGroup `
+  -JobName $jobName `
+  -File $jobTransformationDefinitionFile `
+  -Name $jobTransformationName -Force
